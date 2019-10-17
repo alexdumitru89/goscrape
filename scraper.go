96ed1aba905b94a8f42b9b5029e4c0deeb0e5ce1
root <root@DESKTOP-C32L2MA.localdomain>
2019-10-17 13:55:08 +0300

    added referer

--- scraper.go
+++ scraper.go
@@ -1,34 +1,57 @@
 package main
 
 import (
-	"log"
 	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/proxy"
-	"fmt"
 )
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandomString() string {
+	b := make([]byte, rand.Intn(10)+10)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
 
 func main() {
 
 	var url string
 	var proxies string
+	var ref string
 	flag.StringVar(&url, "url", "", "URL to scrape")
 	flag.StringVar(&proxies, "proxy", "", "Proxy (optional). Syntax: protocol://ip:port")
+	flag.StringVar(&ref, "ref", "", "Referer (optional)")
 	flag.Parse()
-	c := colly.NewCollector()
+	c := colly.NewCollector(colly.UserAgent(RandomString()))
 
-	rp, err := proxy.RoundRobinProxySwitcher(proxies) 
-	if err != nil {
-		log.Fatal(err)
+	if proxies != "" {
+		rp, err := proxy.RoundRobinProxySwitcher(proxies)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.SetProxyFunc(rp)
 	}
- 
-	c.SetProxyFunc(rp)
+
+	c.OnRequest(func(r *colly.Request) {
+		if ref != "" {
+			r.Headers.Set("Referer", "https://www.displayspecifications.com/en")
+		}
+	})
 
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong:", err)
 	})
 
-	c.OnResponse(func(r *colly.Response){
+	c.OnResponse(func(r *colly.Response) {
 		fmt.Println(string(r.Body))
 	})
 
